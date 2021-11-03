c37a87a852fb7d3fcb56659f03553ba6c21e993e
Thu Phan <alicebe.phan@gmail.com>
2021-11-02 20:49:03 -0500

    Set up fields and rules for Tag model.

--- models/Tag.js
+++ models/Tag.js
@@ -4,9 +4,20 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
+// set up fields and rules for Tag model
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    } 
+    
   },
   {
     sequelize,
