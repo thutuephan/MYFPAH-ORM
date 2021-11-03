e0b57946958f4624761918c516caaa60a014514f
Thu Phan <alicebe.phan@gmail.com>
2021-11-03 01:52:19 -0500

    Got the first route 'get api' working up.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,11 +3,12 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   try {
     const categoryData = await Category.findAll({
-      include: [{id}, {category_name}]
+      // `Include` attribute is for foreign keys. Category doesn't have any foreign key; id and category name are already inherent properties of Category, `include category` is not needed in this case
+      include: [Product]
     }) 
     res.status(200).json(categoryData);
   } catch (err) {
@@ -16,18 +17,27 @@ router.get('/', (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
     const categoryData = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
       include: [Product]
-    })
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
   // be sure to include its associated Products
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
