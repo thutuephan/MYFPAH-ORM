e0cc1e1fe590bf004ef47b8143d027678596574c
Thu Phan <alicebe.phan@gmail.com>
2021-11-04 00:34:15 -0500

    Wrote out router.post

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ E-Commerce Back End
 
 
 https://sequelize.org/master/manual/assocs.html
+https://sequelize.org/master/manual/eager-loading.html#fetching-a-single-associated-element
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,17 +7,16 @@ router.get('/', async (req, res) => {
   // find all categories
   try {
     const categoryData = await Category.findAll({
-      // `Include` attribute is for foreign keys. Category doesn't have any foreign key; id and category name are already inherent properties of Category, `include category` is not needed in this case
-      include: [Product]
-    }) 
+      // `Include` attribute is for foreign keys. Category doesn't have any foreign key; id and category name are already inherent properties of Category, `include category` is not needed in this case???
+      // be sure to include its associated Products
+
+      include: [Product],
+     attributes: ['id', 'category_name']
+    })
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
-  // in order for the app to run, I need to run the command `npm run seed` and then `npm start`, in `insomnia`: http:localhost:api/categories or categories/:id
-  //The index.js file adds in the prefix /categories - but the route file won’t show it, so we have to combine them when we use insomnia
-
 });
 
 router.get('/:id', async (req, res) => {
@@ -27,23 +26,35 @@ router.get('/:id', async (req, res) => {
       where: {
         id: req.params.id
       },
+      // without curly braces
       include: [Product]
     });
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-
     res.status(200).json(categoryData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
   // be sure to include its associated Products
+  // in order for the app to run, I need to run the command `npm run seed` and then `npm start`, in `insomnia`: http:localhost:api/categories or categories/:id
+  //The index.js file adds in the prefix /categories - but the route file won’t show it, so we have to combine them when we use insomnia
+
 });
 
-
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 });
 
 router.put('/:id', (req, res) => {
