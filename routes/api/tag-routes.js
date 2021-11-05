2192c27edcf5d4d59be01c5444aa15d2bd5bf2ac
Thu Phan <alicebe.phan@gmail.com>
2021-11-05 17:56:54 -0500

    Wrote out get requests for Tag routes.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,46 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  // include data from the Product model, which is related to each Tag through ProductTag
+
+  try {
+    const tagData = await Tag.findAll({
+      
+          include: [
+            {
+              model: Product, 
+              through: ProductTag,
+            }
+          ]
+    })
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Product,
+          through: ProductTag
+        }
+      ]
+    })
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
