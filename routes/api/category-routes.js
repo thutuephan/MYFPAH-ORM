7e14447897cd9529850efd754b9af590c4386439
Thu Phan <alicebe.phan@gmail.com>
2021-11-05 00:15:34 -0500

    Changed the status code in category and product routs, wrote out delete request for Product route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,7 +63,7 @@ router.put('/:id', async (req, res) => {
       }
     });
     if (!categoryData[0]) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(400).json({ message: 'No category found with this id!' });
       return;
     }
     res.status(200).json(categoryData);
@@ -80,7 +80,7 @@ router.delete('/:id', async (req, res) => {
       }
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(400).json({ message: 'No category found with this id!' });
       return;
     }
     res.status(200).json(categoryData);
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -119,8 +119,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(400).json({message: 'No product was found with this id!'});
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 module.exports = router;
